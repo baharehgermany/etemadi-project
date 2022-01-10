a4e6f8763ad8c0d35c49be1a4fd47b965a332dfb
Bahareh Etemadi <baharehetemadi81@gmail.com>
2022-01-10 14:13:58 +0100

    Added images and started to work on slide show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';//1. functionen importieren
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'etemadi_project';
+
+// Variable deffiniert
+export class AppComponent implements OnInit {// dem component OnInit inplementieren
+images = ['devices.jpg', 'monitor.jpg', 'workplace.jpg'];
+currentImage = 0;
+showImage = true;
+
+  // wird als erstes augerufen
+  ngOnInit(){
+  this.updateImage();
+  }
+
+//images nach alle 8 sec 0, 1, 2, 0, 1, 2 usw.(Modulu ist der Restoperator)
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;//immer einen hinzufügen
+      this.currentImage = this.currentImage % this.images.length;//länge deffinieren mit %
+      this.showImage = false;
+
+      //Interval 100 millisec gestoppt (true für glatte Übergänge)
+      setTimeout(() =>{
+        this.showImage = true;
+
+      }, 10);
+    }, 8000);
+
+  
+  }
+
 }
